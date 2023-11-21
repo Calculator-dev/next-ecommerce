31e06b742dfcd8b0b5d017cee5ea46d72bb4717d
haris <haris.aksalic@zendev.se>
2023-11-21 22:48:40 +0100

    Finished next auth and cart merging when user is logged in and when its logged out

--- src/app/Navbar/Navbar.tsx
+++ src/app/Navbar/Navbar.tsx
@@ -4,6 +4,9 @@ import logo from "../favicon.ico";
 import { redirect } from "next/navigation";
 import { getCart } from "@/lib/db/cart";
 import ShoppingCartButton from "@/app/Navbar/ShoppingCartButton";
+import UserMenuButton from "@/app/Navbar/UserMenuButton";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 async function searchProducts(formData: FormData) {
   "use server";
@@ -11,11 +14,12 @@ async function searchProducts(formData: FormData) {
   const searchQuery = formData.get("searchQuery")?.toString();
 
   if (searchQuery) {
-    redirect("/search?query" + searchQuery);
+    redirect("/search?query=" + searchQuery);
   }
 }
 
 export default async function Navbar() {
+  const session = await getServerSession(authOptions);
   const cart = await getCart();
   return (
     <div className="bg-base-200">
@@ -37,6 +41,7 @@ export default async function Navbar() {
             </div>
           </form>
           <ShoppingCartButton cart={cart} />
+          <UserMenuButton session={session} />
         </div>
       </div>
     </div>
--- src/app/add-product/page.tsx
+++ src/app/add-product/page.tsx
@@ -1,6 +1,8 @@
 import { redirect } from "next/navigation";
 import { FormSubmitButton } from "@/components/FormSubmitButton";
 import { prisma } from "@/lib/db/prisma";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 export const metadata = {
   title: "Add Product - E-commerce",
@@ -8,7 +10,10 @@ export const metadata = {
 
 async function addProduct(formData: FormData) {
   "use server";
-
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    redirect("/api/auth/signin?callbaackUrl=/add-product");
+  }
   const name = formData.get("name")?.toString();
   const description = formData.get("description")?.toString();
   const imageUrl = formData.get("imageUrl")?.toString();
@@ -30,7 +35,13 @@ async function addProduct(formData: FormData) {
   redirect("/");
 }
 
-export default function AddProductPage() {
+export default async function AddProductPage() {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect("/api/auth/signin?callbaackUrl=/add-product");
+  }
+
   return (
     <div>
       <h1 className="text-lg mb-3 font-bold">Add Product</h1>
